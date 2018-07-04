11d83076892ef5d265e0853e93e7f2d5526e3be1
MCMrARM <mcmrarm@gmail.com>
2018-07-04 22:36:54 +0200

    Add debian dependencies (except for EGL)

--- CPackSettings.cmake
+++ CPackSettings.cmake
@@ -9,6 +9,6 @@ set(CPACK_INSTALL_CMAKE_PROJECTS
         "${CMAKE_BINARY_DIR};mcpelauncher-bin-libs;mcpelauncher-bin-libs;/"
         "${CMAKE_CURRENT_BINARY_DIR};mcpelauncher-client;mcpelauncher-client;/")
 set(CPACK_OUTPUT_CONFIG_FILE CPackConfig.cmake)
-set(CPACK_DEBIAN_PACKAGE_DEPENDS "libc6 (>=2.14), libcurl4")
+set(CPACK_DEBIAN_PACKAGE_DEPENDS "libc6 (>=2.14), libstdc++6, libcurl4, libx11-6, zlib1g, libpng16-16, libevdev2, libudev1")
 
 include(CPack)
