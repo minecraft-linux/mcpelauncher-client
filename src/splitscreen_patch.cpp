9306eeddd634598aae386e617b54e285f738a04a
MCMrARM <mcmrarm@gmail.com>
2018-07-12 22:19:31 +0200

    SplitscreenPatch: add compat check

--- src/splitscreen_patch.cpp
+++ src/splitscreen_patch.cpp
@@ -1,6 +1,7 @@
 #include <game_window_manager.h>
 #include <hybris/dlfcn.h>
 #include <mcpelauncher/patch_utils.h>
+#include <log.h>
 #include "splitscreen_patch.h"
 
 void (*SplitscreenPatch::glScissor)(int x, int y, unsigned int w, unsigned int h);
@@ -11,6 +12,10 @@ void SplitscreenPatch::setScissorRect(void*, int x, int y, unsigned int w, unsig
 
 void SplitscreenPatch::install(void* handle) {
     void* ptr = (void*) ((size_t) hybris_dlsym(handle, "_ZN3mce13RenderContext26setViewportWithFullScissorERKNS_12ViewportInfoE") + (0x85E - 0x740));
+    if (*((unsigned char*) ptr) != 0xE8) {
+        Log::error("SplitscreenPatch", "Not patching splitscreen - incompatible code");
+        return;
+    }
     PatchUtils::patchCallInstruction(ptr, (void*) &setScissorRect, false);
 }
 
