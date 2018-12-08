4e1ec8ff0b19d48ebba6bb86b89d69321bd9d9e8
MCMrARM <mcmrarm@gmail.com>
2018-12-08 20:46:11 +0100

    Revert "Patch web_socket_connection::ensure_connected"
    This reverts commit b6e4a574b819b10501350e056a6baa6fa46d4b3f.

--- src/xbox_sleep_shutdown_patch.h
+++ src/xbox_sleep_shutdown_patch.h
@@ -8,15 +8,9 @@ class XboxSleepShutdownPatch {
 private:
     static std::condition_variable cv;
     static std::mutex mutex;
-    static std::mutex ensureConnectedMutex;
     static bool shuttingDown;
 
-    static void (*ensureConnectedOriginal)(void*);
-    static void ensureConnectedHook(void*);
-
     static void sleepHook(unsigned int ms);
-
-    static bool tryInstallWSPatch(void* handle);
 
 public:
     static void install(void* handle);
