72cfba39287ad72b25d1634b5a86a68556b3da42
MCMrARM <mcmrarm@gmail.com>
2018-12-26 20:08:02 +0100

    Print program link errors

--- src/shader_error_patch.cpp
+++ src/shader_error_patch.cpp
@@ -7,16 +7,23 @@
 void (*ShaderErrorPatch::glGetShaderiv)(unsigned int shader, int pname, int* params);
 void (*ShaderErrorPatch::glGetShaderInfoLog)(unsigned int shader, unsigned int maxLength, unsigned int* length, char* log);
 void (*ShaderErrorPatch::glCompileShader)(unsigned int shader);
+void (*ShaderErrorPatch::glGetProgramiv)(unsigned int program, int pname, int* params);
+void (*ShaderErrorPatch::glGetProgramInfoLog)(unsigned int program, unsigned int maxLength, unsigned int* length, char* log);
+void (*ShaderErrorPatch::glLinkProgram)(unsigned int program);
 
 void ShaderErrorPatch::install(void *handle) {
     hybris_hook("glCompileShader", (void*) glCompileShaderHook);
+    hybris_hook("glLinkProgram", (void*) glLinkProgramHook);
 }
 
 void ShaderErrorPatch::onGLContextCreated() {
     auto getProcAddr = GameWindowManager::getManager()->getProcAddrFunc();
     glCompileShader = (void (*)(unsigned int)) getProcAddr("glCompileShader");
+    glLinkProgram = (void (*)(unsigned int)) getProcAddr("glLinkProgram");
     glGetShaderiv = (void (*)(unsigned int, int, int*)) getProcAddr("glGetShaderiv");
     glGetShaderInfoLog = (void (*)(unsigned int, unsigned int, unsigned int*, char*)) getProcAddr("glGetShaderInfoLog");
+    glGetProgramiv = (void (*)(unsigned int, int, int*)) getProcAddr("glGetProgramiv");
+    glGetProgramInfoLog = (void (*)(unsigned int, unsigned int, unsigned int*, char*)) getProcAddr("glGetProgramInfoLog");
 }
 
 void ShaderErrorPatch::glCompileShaderHook(unsigned int shader) {
@@ -29,6 +36,21 @@ void ShaderErrorPatch::glCompileShaderHook(unsigned int shader) {
         glGetShaderiv(shader, /* GL_INFO_LOG_LENGTH */ 0x8B84, (int*) &infoLen);
         char* data = new char[infoLen];
         glGetShaderInfoLog(shader, infoLen, &infoLen, data);
+        printf("%s\n", data); // use printf because the logger may have a restricted length
+        fflush(stdout);
+    }
+}
+
+void ShaderErrorPatch::glLinkProgramHook(unsigned int program) {
+    glLinkProgram(program);
+    int status;
+    glGetProgramiv(program, /* GL_LINK_STATUS */ 0x8B82, &status);
+    if (status != /* GL_TRUE */ 1) {
+        Log::error("Shader", "An error was detected when linking a shader program");
+        unsigned int infoLen;
+        glGetProgramiv(program, /* GL_INFO_LOG_LENGTH */ 0x8B84, (int*) &infoLen);
+        char* data = new char[infoLen];
+        glGetProgramInfoLog(program, infoLen, &infoLen, data);
         printf("%s\n", data); // use printf because the logger may have a restricted length
         fflush(stdout);
     }
--- src/shader_error_patch.h
+++ src/shader_error_patch.h
@@ -11,6 +11,12 @@ private:
     static void (*glCompileShader)(unsigned int shader);
     static void glCompileShaderHook(unsigned int shader);
 
+    static void (*glGetProgramiv)(unsigned int program, int pname, int* params);
+    static void (*glGetProgramInfoLog)(unsigned int program, unsigned int maxLength, unsigned int* length, char* log);
+
+    static void (*glLinkProgram)(unsigned int program);
+    static void glLinkProgramHook(unsigned int program);
+
 public:
     static void install(void* handle);
 
