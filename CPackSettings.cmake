fa58f14f484ce568733e4ed8eceae39504cc7a10
MCMrARM <mcmrarm@gmail.com>
2019-04-25 15:54:36 +0200

    Introduce DEB_OS_NAME

--- CPackSettings.cmake
+++ CPackSettings.cmake
@@ -12,11 +12,14 @@ set(CPACK_INSTALL_CMAKE_PROJECTS
         "${CMAKE_BINARY_DIR};gamepad-mappings;gamepad-mappings;/"
         "${CMAKE_CURRENT_BINARY_DIR};mcpelauncher-client;mcpelauncher-client;/")
 set(CPACK_OUTPUT_CONFIG_FILE CPackConfig.cmake)
-if (DEB_XENIAL_DEPENDENCIES)
+if (DEB_OS_NAME MATCHES "ubuntu-xenial")
     set(CPACK_DEBIAN_PACKAGE_DEPENDS "libc6 (>=2.14), libstdc++6, libcurl3, libx11-6, zlib1g, libpng12-0, libevdev2, libudev1")
     set(CPACK_DEBIAN_PACKAGE_VERSION "${CPACK_DEBIAN_PACKAGE_VERSION}~xenial")
-else()
+elseif(DEB_OS_NAME MATCHES "ubuntu-bionic")
     set(CPACK_DEBIAN_PACKAGE_DEPENDS "libc6 (>=2.14), libstdc++6, libcurl4, libx11-6, zlib1g, libpng16-16, libevdev2, libudev1")
+elseif(DEB_OS_NAME MATCHES "ubuntu-disco")
+    set(CPACK_DEBIAN_PACKAGE_DEPENDS "libc6 (>=2.14), libstdc++6, libcurl4, libx11-6, zlib1g, libpng16-16, libevdev2, libudev1")
+    set(CPACK_DEBIAN_PACKAGE_VERSION "${CPACK_DEBIAN_PACKAGE_VERSION}~disco")
 endif()
 
 include(CPack)
