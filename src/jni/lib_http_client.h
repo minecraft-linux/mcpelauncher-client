f0e5e3b015ee150a809722f1af7067f342623e6a
Adrian Freund <adrian@freund.io>
2020-08-15 10:18:39 +0200

    Implemented com/xbox/httpclient/HttpClientRequest and Response

--- src/jni/lib_http_client.h
+++ src/jni/lib_http_client.h
@@ -2,6 +2,9 @@
 
 #include <fake-jni/fake-jni.h>
 #include "main_activity.h"
+#include <log.h>
+
+#include <utility>
 
 class Ecdsa : public FakeJni::JObject {
 public:
@@ -9,10 +12,18 @@ public:
     Ecdsa();
 };
 
+class ResponseHeader {
+public:
+    ResponseHeader(std::string name, std::string value) : name(std::move(name)), value(std::move(value)) {}
+    std::string name;
+    std::string value;
+};
+
 class HttpClientRequest : public FakeJni::JObject {
 public:
     DEFINE_CLASS_NAME("com/xbox/httpclient/HttpClientRequest")
     HttpClientRequest();
+    ~HttpClientRequest();
 
     static FakeJni::JBoolean isNetworkAvailable(std::shared_ptr<Context> context);
     static std::shared_ptr<HttpClientRequest> createClientRequest();
@@ -21,17 +32,38 @@ public:
     void setHttpMethodAndBody(std::shared_ptr<FakeJni::JString> method, std::shared_ptr<FakeJni::JString> contentType, std::shared_ptr<FakeJni::JByteArray> body);
     void setHttpHeader(std::shared_ptr<FakeJni::JString> name, std::shared_ptr<FakeJni::JString> value);
     void doRequestAsync(FakeJni::JLong sourceCall);
+    static size_t write_callback_wrapper(char *ptr, size_t size, size_t nmemb, void *userdata) {
+        auto *self = static_cast<HttpClientRequest*>(userdata);
+        return self->write_callback(ptr, size, nmemb);
+    }
+    static size_t header_callback_wrapper(char *ptr, size_t size, size_t nmemb, void *userdata) {
+        auto *self = static_cast<HttpClientRequest*>(userdata);
+        return self->header_callback(ptr, size, nmemb);
+    }
+
+private:
+    void *curl;
+    struct curl_slist * header = nullptr;
+    std::vector<signed char> response;
+    std::vector<ResponseHeader> headers;
+
+    size_t write_callback(char *ptr, size_t size, size_t nmemb);
+    size_t header_callback(char *buffer,   size_t size,   size_t nitems);
 };
 
 class HttpClientResponse : public FakeJni::JObject {
 public:
     DEFINE_CLASS_NAME("com/xbox/httpclient/HttpClientResponse")
 
+    HttpClientResponse(int response_code, std::vector<signed char> body, std::vector<ResponseHeader> headers);
     FakeJni::JInt getNumHeaders();
     std::shared_ptr<FakeJni::JString> getHeaderNameAtIndex(FakeJni::JInt index);
     std::shared_ptr<FakeJni::JString> getHeaderValueAtIndex(FakeJni::JInt index);
     std::shared_ptr<FakeJni::JByteArray> getResponseBodyBytes();
     FakeJni::JInt getResponseCode();
 
-
+private:
+    int response_code;
+    std::vector<signed char> body;
+    std::vector<ResponseHeader> headers;
 };
