aa1bf3e1779c9bdbd831487d97738ec46e0f1413
Christopher Homberger <christopher.homberger@web.de>
2020-09-05 11:27:56 +0200

    Fix uuid length

--- src/jni/main_activity.cpp
+++ src/jni/main_activity.cpp
@@ -19,11 +19,12 @@ std::shared_ptr<FakeJni::JString> MainActivity::createUUID() {
     rawBytes[6] = (rawBytes[6] & (unsigned char) 0x0Fu) | (unsigned char) 0x40u;
     rawBytes[8] = (rawBytes[6] & (unsigned char) 0x3Fu) | (unsigned char) 0x80u;
     std::string ret;
-    ret.resize(36);
-    snprintf((char*) ret.c_str(), 36, "%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x",
+    ret.resize(37);
+    snprintf((char*) ret.c_str(), 37, "%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x",
              rawBytes[0], rawBytes[1], rawBytes[2], rawBytes[3],
              rawBytes[4], rawBytes[5], rawBytes[6], rawBytes[7], rawBytes[8], rawBytes[9],
              rawBytes[10], rawBytes[11], rawBytes[12], rawBytes[13], rawBytes[14], rawBytes[15]);
+    ret.resize(36);
     return std::make_shared<FakeJni::JString>(ret);
 }
 
