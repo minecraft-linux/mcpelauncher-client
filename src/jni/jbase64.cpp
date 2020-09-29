06951bde7ab56edeed3b02743b21e0fbc784fa1d
Christopher Homberger <christopher.homberger@web.de>
2020-09-29 12:57:14 +0200

    Fix buffer overflow bug

--- src/jni/jbase64.cpp
+++ src/jni/jbase64.cpp
@@ -9,8 +9,8 @@ std::shared_ptr<FakeJni::JByteArray> JBase64::decode(std::shared_ptr<FakeJni::JS
 }
 
 std::shared_ptr<FakeJni::JByteArray> Arrays::copyOfRange(std::shared_ptr<FakeJni::JByteArray> in, int i, int n) {
-    auto ret = std::make_shared<FakeJni::JByteArray>(n);
-    memcpy(ret->getArray(), in->getArray() + i, n);
+    auto ret = std::make_shared<FakeJni::JByteArray>(n - i);
+    memcpy(ret->getArray(), in->getArray() + i, n - i);
     return ret;
 }
 
