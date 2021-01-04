2a16784ab6784733108caea3591ff9dfec01ef91
Christopher Homberger <christopher.homberger@web.de>
2021-01-04 21:57:20 +0100

    Patch vtable if possible, log an error

--- src/core_patches.cpp
+++ src/core_patches.cpp
@@ -2,6 +2,7 @@
 
 #include <mcpelauncher/linker.h>
 #include <mcpelauncher/patch_utils.h>
+#include <log.h>
 
 std::shared_ptr<GameWindow> CorePatches::currentGameWindow;
 
@@ -9,15 +10,19 @@ void CorePatches::install(void *handle) {
     // void* ptr = linker::dlsym(handle, "_ZN3web4http6client7details35verify_cert_chain_platform_specificERN5boost4asio3ssl14verify_contextERKSs");
     // PatchUtils::patchCallInstruction(ptr, (void*) +[]() { return true; }, true);
 
-
-    void** vta = &((void**) linker::dlsym(handle, "_ZTV21AppPlatform_android23"))[2];
-    PatchUtils::VtableReplaceHelper vtr (handle, vta, vta);
-    vtr.replace("_ZN11AppPlatform16hideMousePointerEv", +[]() {
-        currentGameWindow->setCursorDisabled(true);
-    });
-    vtr.replace("_ZN11AppPlatform16showMousePointerEv", +[]() {
-        currentGameWindow->setCursorDisabled(false);
-    });
+    void * appPlatform = linker::dlsym(handle, "_ZTV21AppPlatform_android23");
+    if(appPlatform) {
+        void** vta = &((void**) appPlatform)[2];
+        PatchUtils::VtableReplaceHelper vtr (handle, vta, vta);
+        vtr.replace("_ZN11AppPlatform16hideMousePointerEv", +[]() {
+            currentGameWindow->setCursorDisabled(true);
+        });
+        vtr.replace("_ZN11AppPlatform16showMousePointerEv", +[]() {
+            currentGameWindow->setCursorDisabled(false);
+        });
+    } else {
+        Log::error("CorePatches", "Failed to patch vtable _ZTV21AppPlatform_android23 not found");
+    }
 }
 
 void CorePatches::setGameWindow(std::shared_ptr<GameWindow> gameWindow) {
