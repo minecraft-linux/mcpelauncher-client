121f28883227477c17ba8adfa693880c167b9317
GameParrot <85067619+GameParrot@users.noreply.github.com>
2023-03-19 13:50:20 -0400

    Fix direct keyboard input on renderdragon versions

--- src/symbols.h
+++ src/symbols.h
@@ -7,14 +7,23 @@ struct Mouse {
 };
 
 struct Keyboard {
+    static bool useLegacyKeyboard;
     struct InputEvent {
         int event;
         unsigned int key;  // it's actually an unsigned char, but the asm code does suspicious stuff with the padding so use an int so it gets zeroed out
         int controllerId;
+        int modShift, modCtrl, modAlt;
+    };
+    struct LegacyInputEvent {
+        int event;
+        unsigned int key;  // it's actually an unsigned char, but the asm code does suspicious stuff with the padding so use an int so it gets zeroed out
+        int controllerId;
+        int modShift, modCtrl, modAlt;
     };
 
     static int* _states;
     static std::vector<Keyboard::InputEvent>* _inputs;
+    static std::vector<Keyboard::LegacyInputEvent>* _inputsLegacy;
     static int* _gameControllerId;
 };
 
