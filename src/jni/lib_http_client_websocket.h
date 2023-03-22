aca3b09ece349fc93559ff48848be16f179ead50
GameParrot <85067619+GameParrot@users.noreply.github.com>
2023-03-22 16:56:45 -0400

    Remove old code

--- src/jni/lib_http_client_websocket.h
+++ src/jni/lib_http_client_websocket.h
@@ -17,10 +17,6 @@ public:
     FakeJni::JBoolean sendMessage(std::shared_ptr<FakeJni::JString>);
     FakeJni::JBoolean sendBinaryMessage(std::shared_ptr<jnivm::ByteBuffer>);
     void disconnect(int id);
-    static size_t write_callback_wrapper(char *ptr, size_t size, size_t nmemb, void *userdata) {
-        auto *self = static_cast<HttpClientWebSocket *>(userdata);
-        return self->write_callback(ptr, size, nmemb);
-    }
     
 private:
     void *curl;
@@ -33,6 +29,4 @@ private:
     static int closesocket(void *clientp, curl_socket_t item);
     void sendOpened();
     void sendClosed();
-    std::vector<signed char> response;
-    std::vector<ResponseHeader> headers;
 };
