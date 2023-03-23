c1cce0deb13a35e1074392c470c19c2a22e54f76
GameParrot <85067619+GameParrot@users.noreply.github.com>
2023-03-23 16:13:27 -0400

    Add HTTP client logging in debug builds

---
+++ lib_http_client.cpp
@@ -0,0 +1,202 @@
+#include "lib_http_client.h"
+#include "../util.h"
+#include <log.h>
+#include <curl/curl.h>
+#include <thread>
+
+using namespace std::placeholders;
+
+HttpClientRequest::HttpClientRequest() {
+    curl = curl_easy_init();
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, this);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, HttpClientRequest::write_callback_wrapper);
+    curl_easy_setopt(curl, CURLOPT_HEADERDATA, this);
+    curl_easy_setopt(curl, CURLOPT_HEADERFUNCTION, HttpClientRequest::header_callback_wrapper);
+}
+
+HttpClientRequest::~HttpClientRequest() {
+    curl_easy_cleanup(curl);
+}
+
+FakeJni::JBoolean HttpClientRequest::isNetworkAvailable(std::shared_ptr<Context> context) {
+    return true;
+}
+
+std::shared_ptr<HttpClientRequest> HttpClientRequest::createClientRequest() {
+    return std::make_shared<HttpClientRequest>();
+}
+
+void HttpClientRequest::setHttpUrl(std::shared_ptr<FakeJni::JString> url) {
+#ifndef NDEBUG
+    Log::trace("HttpClient", "URL: %s", url->asStdString().c_str());
+#endif
+    curl_easy_setopt(curl, CURLOPT_URL, url->asStdString().c_str());
+}
+
+void HttpClientRequest::setHttpMethodAndBody(std::shared_ptr<FakeJni::JString> method,
+                                             std::shared_ptr<FakeJni::JString> contentType,
+                                             std::shared_ptr<FakeJni::JByteArray> body) {
+    this->method = method->asStdString();
+    curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, this->method.c_str());
+    this->body = body ? std::vector<char>((char *)body->getArray(), (char *)body->getArray() + body->getSize()) : std::vector<char>{};
+    if(this->body.size()) {
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, this->body.data());
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, this->body.size());
+    }
+    auto conttype = contentType->asStdString();
+    if(conttype.length()) {
+        header = curl_slist_append(header, ("Content-Type: " + conttype).c_str());
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, header);
+    }
+}
+
+static size_t read_callback(char *ptr, size_t size, size_t nmemb, void *userdata) {
+    auto stream = (NativeInputStream *)userdata;
+    return stream->Read(ptr, size * nmemb);
+}
+
+void HttpClientRequest::setHttpMethodAndBody2(std::shared_ptr<FakeJni::JString> method, FakeJni::JLong callHandle, std::shared_ptr<FakeJni::JString> contentType, FakeJni::JLong contentLength) {
+    this->method = method->asStdString();
+    // if(this->method == "POST") {
+    //     curl_easy_setopt(curl, CURLOPT_HTTPPOST, 1);
+    // } else {
+    if (this->method == "HEAD") {
+        curl_easy_setopt(curl, CURLOPT_NOBODY, 1L);
+    } else {
+        curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, this->method.c_str());
+    }
+    // }
+    if(contentLength > 0) {
+        // static auto ___callback = (void*)+[](char *ptr, size_t size, size_t nmemb, void *userdata) -> size_t {
+        //     auto stream = (NativeInputStream*)userdata;
+        //     return stream->Read(ptr, size * nmemb);
+        // };
+        // this->inputStream = std::make_shared<NativeInputStream>(callHandle);
+        // curl_easy_setopt(curl, CURLOPT_READFUNCTION, read_callback);
+        // curl_easy_setopt(curl, CURLOPT_READDATA, this->inputStream.get());
+        // curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE_LARGE, (curl_off_t) contentLength);
+        this->body.resize(contentLength + 1);
+        auto stream = std::make_shared<NativeInputStream>(callHandle);
+        FakeJni::LocalFrame frame;
+        auto read = stream->Read(this->body.data(), contentLength);
+        this->body[read] = '\0';
+#ifndef NDEBUG
+        Log::trace("HttpClient", "setHttpMethodAndBody2 called, method: %s, body: %s", this->method.c_str(), this->body.data());
+#endif
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, this->body.data());
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, read);
+    }
+    auto conttype = contentType->asStdString();
+    if(conttype.length()) {
+        header = curl_slist_append(header, ("Content-Type: " + conttype).c_str());
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, header);
+    }
+}
+
+void HttpClientRequest::setHttpHeader(std::shared_ptr<FakeJni::JString> name, std::shared_ptr<FakeJni::JString> value) {
+#ifndef NDEBUG
+    Log::trace("HttpClient", "setHttpHeader called, name: %s, value: %s", name->asStdString().c_str(), value->asStdString().c_str());
+#endif
+    header = curl_slist_append(header, (name->asStdString() + ": " + value->asStdString()).c_str());
+    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, header);
+}
+
+void HttpClientRequest::doRequestAsync(FakeJni::JLong sourceCall) {
+    auto me = this->shared_from_this();
+    FakeJni::LocalFrame frame;
+    auto&& jvm = &frame.getJniEnv().getVM();
+    std::thread([=]() {
+        try {
+            auto anotherme = me;
+            auto ret = curl_easy_perform(curl);
+            long response_code;
+            curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
+            FakeJni::LocalFrame frame(*jvm);
+            if(ret == CURLE_OK) {
+#ifndef NDEBUG
+                Log::trace("HttpClient", "Response: code: %d, body: %s", response_code, response.data());
+#endif
+                auto method = getClass().getMethod("(JLcom/xbox/httpclient/HttpClientResponse;)V", "OnRequestCompleted");
+                method->invoke(frame.getJniEnv(), this, sourceCall, frame.getJniEnv().createLocalReference(std::make_shared<HttpClientResponse>(sourceCall, response_code, response, headers)));
+            } else {
+                auto method = getClass().getMethod("(JLjava/lang/String;)V", "OnRequestFailed");
+                method->invoke(frame.getJniEnv(), this, sourceCall, frame.getJniEnv().createLocalReference(std::make_shared<FakeJni::JString>("Error")));
+            }
+        } catch(...) {
+            FakeJni::LocalFrame frame(*jvm);
+            auto method = getClass().getMethod("(JLjava/lang/String;)V", "OnRequestFailed");
+            method->invoke(frame.getJniEnv(), this, sourceCall, frame.getJniEnv().createLocalReference(std::make_shared<FakeJni::JString>("Error")));
+        }
+    }).detach();
+}
+
+FakeJni::JInt HttpClientResponse::getNumHeaders() {
+    return headers.size();
+}
+
+std::shared_ptr<FakeJni::JString> HttpClientResponse::getHeaderNameAtIndex(FakeJni::JInt index) {
+    return std::make_shared<FakeJni::JString>(headers[index].name);
+}
+
+std::shared_ptr<FakeJni::JString> HttpClientResponse::getHeaderValueAtIndex(FakeJni::JInt index) {
+    return std::make_shared<FakeJni::JString>(headers[index].value);
+}
+
+std::shared_ptr<FakeJni::JByteArray> HttpClientResponse::getResponseBodyBytes() {
+    return std::make_shared<FakeJni::JByteArray>(body);
+}
+
+void HttpClientResponse::getResponseBodyBytes2() {
+    std::make_shared<NativeOutputStream>(call_handle)->WriteAll(getResponseBodyBytes());
+}
+
+FakeJni::JInt HttpClientResponse::getResponseCode() {
+    return response_code;
+}
+
+HttpClientResponse::HttpClientResponse(FakeJni::JLong call_handle, int response_code, std::vector<signed char> body, std::vector<ResponseHeader> headers) : response_code(response_code),
+                                                                                                                                                            body(body),
+                                                                                                                                                            headers(headers),
+                                                                                                                                                            call_handle(call_handle) {}
+
+size_t HttpClientRequest::write_callback(char *ptr, size_t size, size_t nmemb) {
+    response.insert(response.end(), ptr, ptr + nmemb);
+    return size * nmemb;
+}
+
+size_t HttpClientRequest::header_callback(char *buffer, size_t size, size_t nitems) {
+    auto string = std::string(buffer, nitems);
+    auto location = string.find(": ");
+    if(location != std::string::npos) {
+        auto name = string.substr(0, location);
+        auto value = string.substr(location + 1, string.length());
+        trim(name);
+        trim(value);
+        headers.emplace_back(name, value);
+    }
+    return size * nitems;
+}
+
+NativeInputStream::NativeInputStream(FakeJni::JLong call_handle) : call_handle(call_handle) {
+}
+
+size_t NativeInputStream::Read(void *buffer, size_t size) {
+    FakeJni::LocalFrame frame;
+    auto method = getClass().getMethod("(JJ[BJJ)I", "nativeRead");
+    auto buf = std::make_shared<FakeJni::JByteArray>((size_t)std::numeric_limits<jsize>::max() < size ? std::numeric_limits<jsize>::max() : (jsize)size);
+    jvalue ret = method->invoke(frame.getJniEnv(), this, call_handle, offset, frame.getJniEnv().createLocalReference(buf), (FakeJni::JLong)0, (FakeJni::JLong)buf->getSize());
+    if(ret.i != -1) {
+        memcpy(buffer, buf->getArray(), ret.i);
+        offset += ret.i;
+    }
+    return ret.i;
+}
+
+NativeOutputStream::NativeOutputStream(FakeJni::JLong call_handle) : call_handle(call_handle) {
+}
+
+void NativeOutputStream::WriteAll(std::shared_ptr<FakeJni::JByteArray> data) {
+    FakeJni::LocalFrame frame;
+    auto method = getClass().getMethod("(J[BII)V", "nativeWrite");
+    method->invoke(frame.getJniEnv(), this, call_handle, frame.getJniEnv().createLocalReference(data), (FakeJni::JInt)0, (FakeJni::JInt)data->getSize());
+}
---
+++ lib_http_client_websocket.cpp
@@ -0,0 +1,150 @@
+#include "lib_http_client_websocket.h"
+#include "../util.h"
+#include <log.h>
+#include <curl/curl.h>
+#include <thread>
+#include <unistd.h>
+
+#if defined(CURLWS_TEXT) && defined(CURLWS_BINARY)
+#define ENABLE_WEBSOCKETS
+#endif
+
+HttpClientWebSocket::HttpClientWebSocket(FakeJni::JLong owner) {
+    HttpClientWebSocket::owner = owner;
+
+#ifdef ENABLE_WEBSOCKETS
+    curl = curl_easy_init();
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, this);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writecb);
+#endif
+
+    jvm = (void*)&FakeJni::JniEnvContext().getJniEnv().getVM();
+}
+HttpClientWebSocket::~HttpClientWebSocket() {
+#ifdef ENABLE_WEBSOCKETS
+    curl_slist_free_all(header);
+    curl_easy_cleanup(curl);
+#endif
+}
+
+void HttpClientWebSocket::connect(std::shared_ptr<FakeJni::JString> url, std::shared_ptr<FakeJni::JString> wst) {
+#ifndef NDEBUG
+    Log::trace("HttpClientWebSocket", "connect called, url: %s, wst: %s", url->asStdString().c_str(), wst->asStdString().c_str());
+#endif
+    std::thread([=]() {
+#ifdef ENABLE_WEBSOCKETS
+        curl_easy_setopt(curl, CURLOPT_URL, url->asStdString().c_str());
+        header = curl_slist_append(header, ("Sec-WebSocket-Protocol: " + wst->asStdString()).data());
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, header);
+        auto ret = curl_easy_perform(curl);
+        if(ret == CURLE_OK) {
+            Log::error("HTTPClientWebSocket", "websocket connection closed");
+            sendClosed();
+        } else {
+            Log::error("HTTPClientWebSocket", "websocket connection closed with an error");
+            connected = false;
+            FakeJni::LocalFrame frame(*(FakeJni::Jvm*)jvm);
+            auto method = getClass().getMethod("()V", "onFailure");
+            method->invoke(frame.getJniEnv(), this);
+        }
+#else
+        Log::error("HTTPClientWebSocket", "Missing curl websocket support");
+        sendOpened();
+#endif
+    }).detach();
+}
+
+void HttpClientWebSocket::addHeader(std::shared_ptr<FakeJni::JString> name, std::shared_ptr<FakeJni::JString> value) {
+#ifndef NDEBUG
+    Log::trace("HttpClientWebSocket", "addHeader called, name: %s, value: %s", name->asStdString().c_str(), value->asStdString().c_str());
+#endif
+    header = curl_slist_append(header, (name->asStdString() + ": " + value->asStdString()).c_str());
+}
+
+FakeJni::JBoolean HttpClientWebSocket::sendMessage(std::shared_ptr<FakeJni::JString> msg) {
+#ifndef NDEBUG
+    Log::trace("HttpClientWebSocket", "sendMessage called, message: %s", msg->asStdString().c_str());
+#endif
+#ifdef ENABLE_WEBSOCKETS
+    if(!connected) {
+        return false;
+    }
+    size_t sent = 0;
+    curl_ws_send(curl, msg->asStdString().c_str(), msg->asStdString().length(), &sent, 0, CURLWS_TEXT);
+#endif
+    return true;
+}
+
+FakeJni::JBoolean HttpClientWebSocket::sendBinaryMessage(std::shared_ptr<jnivm::ByteBuffer> msg) {
+#ifndef NDEBUG
+    Log::trace("HttpClientWebSocket", "sendBinaryMessage called");
+#endif
+#ifdef ENABLE_WEBSOCKETS
+    if(!connected) {
+        return false;
+    }
+    size_t sent = 0;
+    curl_ws_send(curl, msg->buffer, msg->capacity, &sent, 0, CURLWS_BINARY);
+#endif
+    return true;
+}
+
+void HttpClientWebSocket::disconnect(int id) {
+#ifndef NDEBUG
+    Log::trace("HttpClientWebSocket", "disconnect called, id: %d", id);
+#endif
+    connected = false;
+    size_t sent;
+    curl_ws_send(curl, "", 0, &sent, 0, CURLWS_CLOSE);
+}
+
+size_t HttpClientWebSocket::write_callback(char *ptr, size_t size, size_t nmemb) {
+#ifdef ENABLE_WEBSOCKETS
+    if(!connected) {
+        sendOpened();
+    }
+    auto frame = curl_ws_meta(curl);
+    if(frame->flags & CURLWS_TEXT) {
+        std::string cont((const char*)ptr, nmemb);
+#ifndef NDEBUG
+        Log::trace("HttpClientWebSocket", "Got message: %s", cont.c_str());
+#endif
+        FakeJni::LocalFrame frame(*(FakeJni::Jvm*)jvm);
+        auto method = getClass().getMethod("(Ljava/lang/String;)V", "onMessage");
+        method->invoke(frame.getJniEnv(), this, frame.getJniEnv().createLocalReference(std::make_shared<FakeJni::JString>(cont)));
+    } else if(frame->flags & CURLWS_BINARY) {
+#ifndef NDEBUG
+        Log::trace("HttpClientWebSocket", "Got binary message");
+#endif
+        FakeJni::LocalFrame frame(*(FakeJni::Jvm*)jvm);
+        auto method = getClass().getMethod("(Ljava/nio/ByteBuffer;)V", "onBinaryMessage");
+        method->invoke(frame.getJniEnv(), this, frame.getJniEnv().createLocalReference(std::make_shared<jnivm::ByteBuffer>(ptr, nmemb)));
+    }
+#endif
+    return nmemb;
+}
+
+
+size_t HttpClientWebSocket::writecb(char *buffer, size_t size, size_t nitems, void *userdata) {
+    return ((HttpClientWebSocket*)userdata)->write_callback(buffer, size, nitems);
+}
+
+void HttpClientWebSocket::sendOpened() {
+#ifndef NDEBUG
+    Log::trace("HttpClientWebSocket", "Sending onOpen");
+#endif
+    FakeJni::LocalFrame frame(*(FakeJni::Jvm*)jvm);
+    auto method = getClass().getMethod("()V", "onOpen");
+    method->invoke(frame.getJniEnv(), this);
+    connected = true;
+}
+
+void HttpClientWebSocket::sendClosed() {
+#ifndef NDEBUG
+    Log::trace("HttpClientWebSocket", "Sending onClose");
+#endif
+    connected = false;
+    FakeJni::LocalFrame frame(*(FakeJni::Jvm*)jvm);
+    auto method = getClass().getMethod("(I)V", "onClose");
+    method->invoke(frame.getJniEnv(), this, 0);
+}
