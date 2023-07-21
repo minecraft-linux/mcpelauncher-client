b84b1d1353764930e60f47179a2383f9fce386fb
GameParrot <85067619+GameParrot@users.noreply.github.com>
2023-07-21 14:48:14 -0400

    Set SDK_INT to 28

--- src/jni/main_activity.cpp
+++ src/jni/main_activity.cpp
@@ -15,7 +15,7 @@
 
 #include <log.h>
 
-FakeJni::JInt BuildVersion::SDK_INT = 27;
+FakeJni::JInt BuildVersion::SDK_INT = 28;
 std::shared_ptr<FakeJni::JString> BuildVersion::RELEASE = std::make_shared<FakeJni::JString>("AndroidX");
 
 std::shared_ptr<FakeJni::JString> MainActivity::createUUID() {
