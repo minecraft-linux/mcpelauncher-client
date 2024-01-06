205bd59bc0a9d7d8aad0d12afc0db60690c91182
Christopher Homberger <christopher.homberger@web.de>
2024-01-06 22:35:03 +0100

    imgui

--- src/fake_egl.cpp
+++ src/fake_egl.cpp
@@ -8,10 +8,14 @@
 #include <log.h>
 #include <cstring>
 #include <game_window.h>
+#include <game_window_manager.h>
 #include <mcpelauncher/linker.h>
 #ifdef USE_ARMHF_SUPPORT
 #include "armhf_support.h"
 #endif
+
+#include <imgui.h>
+#include <imgui_impl_opengl3.h>
 
 namespace fake_egl {
 
@@ -92,18 +96,137 @@ EGLBoolean eglDestroyContext(EGLDisplay display, EGLContext context) {
     return EGL_TRUE;
 }
 
+static std::string g_IniFilename;
+
 EGLBoolean eglMakeCurrent(EGLDisplay display, EGLSurface draw, EGLSurface read, EGLContext context) {
     if(draw != nullptr) {
         ((GameWindow *)draw)->makeCurrent(true);
+        if(g_IniFilename.empty()) {
+            // Setup Dear ImGui context
+            IMGUI_CHECKVERSION();
+            ImGui::CreateContext();
+            ImGuiIO& io = ImGui::GetIO();
+
+            // Redirect loading/saving of .ini file to our location.
+            // Make sure 'g_IniFilename' persists while we use Dear ImGui.
+            g_IniFilename = std::string("/tmp") + "/imgui.ini";
+            io.IniFilename = g_IniFilename.c_str();;
+
+            // Setup Dear ImGui style
+            ImGui::StyleColorsDark();
+            //ImGui::StyleColorsLight();
+
+            // Setup Platform/Renderer backends
+            // ImGui_ImplAndroid_Init(g_App->window);
+            // ImGuiIO& io = ImGui::GetIO();
+            io.BackendPlatformName = "imgui_impl_android";
+
+            ImGui_ImplOpenGL3_Init("#version 100");
+
+            // Load Fonts
+            // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
+            // - If the file cannot be loaded, the function will return a nullptr. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
+            // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
+            // - Read 'docs/FONTS.md' for more instructions and details.
+            // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
+            // - Android: The TTF files have to be placed into the assets/ directory (android/app/src/main/assets), we use our GetAssetData() helper to retrieve them.
+
+            // We load the default font with increased size to improve readability on many devices with "high" DPI.
+            // FIXME: Put some effort into DPI awareness.
+            // Important: when calling AddFontFromMemoryTTF(), ownership of font_data is transfered by Dear ImGui by default (deleted is handled by Dear ImGui), unless we set FontDataOwnedByAtlas=false in ImFontConfig
+            ImFontConfig font_cfg;
+            font_cfg.SizePixels = 22.0f;
+            io.Fonts->AddFontDefault(&font_cfg);
+            //void* font_data;
+            //int font_data_size;
+            //ImFont* font;
+            //font_data_size = GetAssetData("segoeui.ttf", &font_data);
+            //font = io.Fonts->AddFontFromMemoryTTF(font_data, font_data_size, 16.0f);
+            //IM_ASSERT(font != nullptr);
+            //font_data_size = GetAssetData("DroidSans.ttf", &font_data);
+            //font = io.Fonts->AddFontFromMemoryTTF(font_data, font_data_size, 16.0f);
+            //IM_ASSERT(font != nullptr);
+            //font_data_size = GetAssetData("Roboto-Medium.ttf", &font_data);
+            //font = io.Fonts->AddFontFromMemoryTTF(font_data, font_data_size, 16.0f);
+            //IM_ASSERT(font != nullptr);
+            //font_data_size = GetAssetData("Cousine-Regular.ttf", &font_data);
+            //font = io.Fonts->AddFontFromMemoryTTF(font_data, font_data_size, 15.0f);
+            //IM_ASSERT(font != nullptr);
+            //font_data_size = GetAssetData("ArialUni.ttf", &font_data);
+            //font = io.Fonts->AddFontFromMemoryTTF(font_data, font_data_size, 18.0f, nullptr, io.Fonts->GetGlyphRangesJapanese());
+            //IM_ASSERT(font != nullptr);
+
+            // Arbitrary scale-up
+            // FIXME: Put some effort into DPI awareness
+            ImGui::GetStyle().ScaleAllSizes(3.0f);
+        }
     } else {
         ((GameWindow *)currentDrawSurface)->makeCurrent(false);
     }
     currentDrawSurface = draw;
     return EGL_TRUE;
 }
+#include <time.h>
+static double                                   g_Time = 0.0;
 
 EGLBoolean eglSwapBuffers(EGLDisplay display, EGLSurface surface) {
     //    Log::trace("FakeEGL", "eglSwapBuffers");
+    ImGuiIO& io = ImGui::GetIO();
+    // Start the Dear ImGui frame
+    ImGui_ImplOpenGL3_NewFrame();
+    //ImGui_ImplAndroid_NewFrame();
+
+        // Setup display size (every frame to accommodate for window resizing)
+    int32_t window_width;
+    int32_t window_height;
+    ((GameWindow *)surface)->getWindowSize(window_width, window_height);
+    int display_width = window_width;
+    int display_height = window_height;
+
+    io.DisplaySize = ImVec2((float)window_width, (float)window_height);
+    if (window_width > 0 && window_height > 0)
+        io.DisplayFramebufferScale = ImVec2((float)display_width / window_width, (float)display_height / window_height);
+
+    // Setup time step
+    struct timespec current_timespec;
+    clock_gettime(CLOCK_MONOTONIC, &current_timespec);
+    double current_time = (double)(current_timespec.tv_sec) + (current_timespec.tv_nsec / 1000000000.0);
+    io.DeltaTime = g_Time > 0.0 ? (float)(current_time - g_Time) : (float)(1.0f / 60.0f);
+    g_Time = current_time;
+
+    ImGui::NewFrame();
+
+    // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+    {
+        static float f = 0.0f;
+        static int counter = 0;
+
+        ImGui::Begin("Hello, world!"); // Create a window called "Hello, world!" and append into it.
+
+        ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+
+        ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+
+        if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+            counter++;
+        ImGui::SameLine();
+        ImGui::Text("counter = %d", counter);
+
+        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+        ImGui::End();
+    }
+
+    // Rendering
+    ImGui::Render();
+    
+    void (*glViewport)(int x, int y, int width, int height) = (decltype(glViewport)) GameWindowManager::getManager()->getProcAddrFunc()("glViewport");
+
+    glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
+    // glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+    // glClear(GL_COLOR_BUFFER_BIT);
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    // eglSwapBuffers(g_EglDisplay, g_EglSurface);
+
     ((GameWindow *)surface)->swapBuffers();
     return EGL_TRUE;
 }
