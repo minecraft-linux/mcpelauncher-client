bedda896432e458abaae661a876970f3761e7674
ChristopherHX <christopher.homberger@web.de>
2024-01-26 20:00:57 +0100

    refactor: xal openssl (#85)
    - resolve use after free
    - deallocate BigNum
    - implement securerandom

--- src/jni/securerandom.cpp
+++ src/jni/securerandom.cpp
@@ -1,5 +1,8 @@
 #include "securerandom.h"
+#include <openssl/rand.h>
 
 std::shared_ptr<FakeJni::JByteArray> SecureRandom::GenerateRandomBytes(int bytes) {
-    return std::make_shared<FakeJni::JByteArray>(bytes);
+    auto randomBytes = std::make_shared<FakeJni::JByteArray>(bytes);
+    RAND_bytes((unsigned char *)randomBytes->getArray(), bytes);
+    return randomBytes;
 }
